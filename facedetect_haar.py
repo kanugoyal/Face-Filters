693ccb8c9978bdee4e9d49199c3ba67f039ed698
kanugoyal <kanugoyal716@gmail.com>
2022-10-16 00:18:14 +0530

    face detection using haar cascade classifier

---
+++ facedetect_haar.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+#from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('Haar_Cascade Classifier',frame)
+    if cv2.waitKey(10) == ord('q'):
+        break
+    
